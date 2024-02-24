28663f29e7322b7cab111c1ef4cbdcd10552a137
Brandon Moye <brandon.m.moye@gmail.com>
2024-02-24 13:20:57 -0600

    linking to my app and function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useContext } from 'react';
 
+import { DataContext } from './Providers/Data'
 
 function App() {
+  const {
+    data, isLoadingData
+  } = useContext(DataContext)
 
   return (
-    <>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {isLoadingData ? 'I am loading data...' : 'Data has been loaded!'}
+        </p>
 
-    </>
-  )
+        <p>
+          {data ? 'did I get it' : 'I dont go it'}
+        </p>
+        <p>
+          {data.name}
+        </p>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Helpers/Mongo.jsx
@@ -0,0 +1,25 @@
+import * as Realm from "realm-web";
+
+// MongoDB Setup
+// https://docs.mongodb.com/guides/realm/react-web-quickstart/
+// https://docs.mongodb.com/realm/sdk/node/examples/authenticate-users/#anonymous-user
+const REALM_APP_ID = "application-0-dminr"; // e.g. myrealm-abcde
+const realm = new Realm.App({ id: REALM_APP_ID });
+
+async function doLoginRealm() {
+    // https://www.mongodb.com/docs/realm/sdk/node/examples/authenticate-users/#custom-function-user
+    let creds = Realm.Credentials.anonymous()
+
+    return await realm.logIn(creds);
+}
+
+async function doGetData() {
+    // TODO: define a function in realm called getData
+    return await realm.currentUser.functions.getData({});
+}
+
+export {
+    realm,
+    doLoginRealm,
+    doGetData
+}
